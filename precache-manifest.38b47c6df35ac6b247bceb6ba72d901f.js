61e5c28837f102ba8517ce90cea3cb39fd221c60
sathyamurali <sathya.murali@gmail.com>
2019-01-23 22:59:13 +0530

    Add files via upload

---
+++ precache-manifest.38b47c6df35ac6b247bceb6ba72d901f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b415209034f8a1c49076",
+    "url": "/js/sai.a7f29926.js"
+  },
+  {
+    "revision": "33633d0620618602c12c",
+    "url": "/js/chunk-vendors.a3a0abcb.js"
+  },
+  {
+    "revision": "68b4e9d5d7b60fc46e97",
+    "url": "/js/app.cdb36b5d.js"
+  },
+  {
+    "revision": "5b4d83382f46880ccf246f9346ad9b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "33633d0620618602c12c",
+    "url": "/css/chunk-vendors.2510fb21.css"
+  },
+  {
+    "revision": "68b4e9d5d7b60fc46e97",
+    "url": "/css/app.da95c417.css"
+  }
+];
